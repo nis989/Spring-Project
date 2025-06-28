8b2208704efdcbb42f5ccda2ddb40ca441f9fe07
macbook <nishantyadav321.ni@gmail.com>
2025-06-28 23:56:04 +0530

    telegrambot added

---
+++ Spring-Project/demo/src/main/java/com/nishu/telegram/model/TelegramRequest.java
@@ -0,0 +1,22 @@
+package com.nishu.telegram.model;
+
+public class TelegramRequest {
+	private String chatId;
+	private String message;
+
+	public String getChatId() {
+		return chatId;
+	}
+
+	public void setChatId(String chatId) {
+		this.chatId = chatId;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
